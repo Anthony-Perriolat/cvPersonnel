6885b7e43649fac56a298687c868888583119eba
Anthony-Perriolat <pro.aperriolat@gmail.com>
2021-10-23 14:12:01 +0200

    Add: Countdown and color background

--- js/js.js
+++ js/js.js
@@ -1,9 +1,42 @@
-let dateStart = new Date(2020, 11, 30, 8, 30);
-let dateEnd = Date.now();
-$('#start').append(dateStart)
-setInterval( () => {
-    let dateEnd = Date.now();
-    let ms = Math.abs(dateStart - dateEnd)
-    console.log(ms)
-    $('#sinceNow').live($('#sinceNow').replaceWith(ms))
-}, 1000)
+function getTimeSince(dateStart) {
+    let now = Date.now()
+    const total = now - Date.parse(dateStart);
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+console.log(total)
+    return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds
+    };
+}
+
+function initializeClock(id, dateStart) {
+    const clock = document.getElementById(id);
+    const daysSpan = clock.querySelector('.days');
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        const t = getTimeSince(dateStart);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+}
+const dateStart = new Date(2020, 10, 30, 8, 30);
+initializeClock('clockdiv', dateStart);
